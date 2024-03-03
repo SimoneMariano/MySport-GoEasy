4ecdd083784f2342e3e15a983f381ee1158a6cfe
Simone Mariano <simone.mariano1301@gmail.com>
2024-03-03 17:43:53 +0100

    Spostata sezione contattaci della home in about, inserita icona paypal in donate in home, modificata midbar e footbar in home, fix di alcuni link, fix script per lo scroll al click di download button in home

--- js/scroll.js
+++ js/scroll.js
@@ -1,17 +1,36 @@
 function scrollToSection() {
   var newWindow = window.open("/html/info.html");
 
-  // Gestisce l'evento di messaggio ricevuto dalla nuova pagina
-  window.addEventListener("message", function (event) {
-    if (event.data === "scrollToElement") {
-      var targetElement = newWindow.document.getElementById("download");
-      var targetOffset = targetElement.offsetTop;
+  // Ascolta l'evento 'load' sulla finestra appena aperta
+  newWindow.addEventListener("load", function () {
+    // Aspetta 500 millisecondi prima di cercare l'elemento e scorrere
+    setTimeout(function () {
+      // Assicurati che la finestra figlia sia completamente caricata
+      if (newWindow.document.readyState === "complete") {
+        // Ora puoi accedere al DOM della finestra figlia in modo sicuro
+        var targetElement = newWindow.document.getElementById("download");
+        if (targetElement) {
+          var targetOffset = targetElement.offsetTop;
 
-      newWindow.scrollTo({
-        top: targetOffset,
-        behavior: "smooth",
-      });
-    }
+          if ("scrollBehavior" in document.documentElement.style) {
+            // Il browser supporta lo scrolling smooth
+            newWindow.scrollTo({
+              top: targetOffset,
+              behavior: "smooth",
+            });
+          } else {
+            // Fallback per lo scrolling normale senza effetto smooth
+            newWindow.scrollTo(0, targetOffset);
+          }
+        } else {
+          console.error(
+            "L'elemento #download non è stato trovato nella finestra figlia."
+          );
+        }
+      } else {
+        console.error("La finestra figlia non è stata completamente caricata.");
+      }
+    }, 5); // Attendi 500 millisecondi prima di eseguire lo scrolling
   });
 
   // Invia un messaggio alla nuova pagina per richiedere lo scroll
